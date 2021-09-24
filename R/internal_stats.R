ad41c30d8dbf491c9e359990b18e0b7296e4e468
leonardblaschek <l.blaschek@vivaldi.net>
2021-09-24 15:07:22 +0200

    fix #2

--- R/internal_stats.R
+++ R/internal_stats.R
@@ -10,87 +10,141 @@
 #' @param print_adjust Adjustment of the letter position multiples of the overall standard deviations. Defaults to 1.
 #' @param stat_alpha The significance threshold alpha. Defaults to 0.05.
 #' @param p_adj_method Method for p value adjustment, see ?dunn.test::dunn.test for details. One of "none", "bonferroni", "sidak", "holm", "hs", "hochberg", "bh", "by". Defaults to "holm".
+#' @param table A logical value indicating whether to return a data frame with the letter codes according to significance (table = FALSE, default), or a table containing the calculated statistics (P-values, df, etc.).
 #' @keywords internal
 #' @export
 
-internal_stats <- function(
-                           df,
+internal_stats <- function(df,
                            y,
                            x,
                            stat_method,
                            print_position,
                            print_adjust,
                            stat_alpha,
-                           p_adj_method) {
-  # non-parametric Kruskal-Wallis test with Dunn's test for multiple comparisons
-  if (stat_method == "kruskal") {
-    dunn <- tibble::as_tibble(dunn.test::dunn.test(
-      unlist(df[dplyr::quo_name(y)]),
-      unlist(df[dplyr::quo_name(x)]),
-      method = p_adj_method
-    )) %>%
-      # remove spaces to meet multcompView input requirements
-      mutate(comparisons = str_replace(comparisons, " - ", "-"))
-
-    # transform adjusted P values to a named vector
-    dunn_vector <- dunn %>%
-      dplyr::select(P.adjusted) %>%
-      tibble::deframe()
-    names(dunn_vector) <- dunn$comparisons
-
-    # calculate letter codes
-    model_formula <- formula(paste0(dplyr::quo_name(y), "~", dplyr::quo_name(x)))
-    groups <- data.frame(
-      multcompView::multcompLetters2(
-        model_formula,
-        dunn_vector,
-        threshold = stat_alpha,
-        data = data.frame(df)
-      )["Letters"]
-    )
-  } else if (stat_method == "tukey") {
-    # parametric Tukey HSD test
-    model_formula <- formula(paste0(dplyr::quo_name(y), "~", dplyr::quo_name(x)))
-    aov1 <- aov(model_formula, data = df)
-    base_tukey <- TukeyHSD(
-      aov1,
-      dplyr::quo_name(x)
-    )
-    # calculate letter codes
-    groups <- data.frame(
-      multcompView::multcompLetters2(
-        model_formula,
-        base_tukey[[dplyr::quo_name(x)]][, 4],
-        threshold = stat_alpha,
-        data = data.frame(df)
-      )["Letters"]
-    )
+                           p_adj_method,
+                           table) {
+  if (table == TRUE) {
+    if (stat_method == "kruskal") {
+      kruskal <- kruskal.test(
+        unlist(df[dplyr::quo_name(y)]),
+        unlist(df[dplyr::quo_name(x)]))
+      dunn <- tibble::as_tibble(dunn.test::dunn.test(
+        unlist(df[dplyr::quo_name(y)]),
+        unlist(df[dplyr::quo_name(x)]),
+        method = p_adj_method
+      )) %>%
+        # remove spaces to meet multcompView input requirements
+        mutate(comparisons = stringr::str_replace_all(comparisons, " - ", "-"))
+      
+      # kruskal statistics
+      kruskal_tab <- broom::tidy(kruskal) %>% 
+        dplyr::select(statistic, "df" = parameter, p.value) %>% 
+        dplyr::mutate(method = "Kruskal-Wallis test (non-parametric)")
+      # dunn comparisons
+      dunn_tab <- dunn %>% 
+        dplyr::select("contrast" = comparisons, 
+                      "adj.p.value" = P.adjusted) %>% 
+        dplyr::mutate(method = "Dunn test (non-parametric)",
+                      contrast = stringr::str_replace_all(contrast, "PLACEHOLDER", "-"))
+      table <- tibble::tibble("kruskal" = kruskal_tab,
+                              "dunn" = dunn_tab)
+    } else if (stat_method == "tukey") {
+      # parametric Tukey HSD test
+      model_formula <- formula(paste0(dplyr::quo_name(y), "~", dplyr::quo_name(x)))
+      aov1 <- aov(model_formula, data = df)
+      base_tukey <- TukeyHSD(
+        aov1,
+        dplyr::quo_name(x)
+      )
+      # anova statistics
+      anova_tab <- broom::tidy(aov1) %>% 
+        dplyr::filter(term == dplyr::quo_name(x)) %>% 
+        dplyr::select(statistic, df, p.value) %>% 
+        dplyr::mutate(method = "ANOVA (parametric)")
+      # tukey comparisons
+      tukey_tab <- broom::tidy(base_tukey) %>% 
+        dplyr::select(contrast, adj.p.value) %>% 
+        dplyr::mutate(method = "Tukey HSD test (parametric)",
+                      contrast = stringr::str_replace_all(contrast, "PLACEHOLDER", "-"))
+      table <- tibble::tibble("anova" = anova_tab,
+                              "tukey" = tukey_tab)
+    } else {
+      stop("Specify a method for the statistical analysis (either 'tukey' or 'kruskal')")
+    }
+    return(table)
   } else {
-    stop("Specify a method for the statistical analysis (either 'tukey' or 'kruskal')")
+    # non-parametric Kruskal-Wallis test with Dunn's test for multiple comparisons
+    if (stat_method == "kruskal") {
+      dunn <- tibble::as_tibble(dunn.test::dunn.test(
+        unlist(df[dplyr::quo_name(y)]),
+        unlist(df[dplyr::quo_name(x)]),
+        method = p_adj_method
+      )) %>%
+        # remove spaces to meet multcompView input requirements
+        mutate(comparisons = str_replace(comparisons, " - ", "-"))
+      
+      # transform adjusted P values to a named vector
+      dunn_vector <- dunn %>%
+        dplyr::select(P.adjusted) %>%
+        tibble::deframe()
+      names(dunn_vector) <- dunn$comparisons
+      
+      # calculate letter codes
+      model_formula <- formula(paste0(dplyr::quo_name(y), "~", dplyr::quo_name(x)))
+      groups <- data.frame(
+        multcompView::multcompLetters2(
+          model_formula,
+          dunn_vector,
+          threshold = stat_alpha,
+          data = data.frame(df)
+        )["Letters"]
+      )
+    } else if (stat_method == "tukey") {
+      # parametric Tukey HSD test
+      model_formula <- formula(paste0(dplyr::quo_name(y), "~", dplyr::quo_name(x)))
+      aov1 <- aov(model_formula, data = df)
+      base_tukey <- TukeyHSD(
+        aov1,
+        dplyr::quo_name(x)
+      )
+      # calculate letter codes
+      groups <- data.frame(
+        multcompView::multcompLetters2(
+          model_formula,
+          base_tukey[[dplyr::quo_name(x)]][, 4],
+          threshold = stat_alpha,
+          data = data.frame(df)
+        )["Letters"]
+      )
+    } else {
+      stop("Specify a method for the statistical analysis (either 'tukey' or 'kruskal')")
+    }
+    raw_sd <- df %>% dplyr::summarise(sd = sd(!!y, na.rm = TRUE))
+    if (print_position == "above") {
+      print_position_calc <- df %>%
+        dplyr::group_by(!!x) %>%
+        dplyr::summarise(!!y := max(!!y, na.rm = TRUE) + print_adjust * raw_sd$sd)
+      groups[[dplyr::quo_name(x)]] <- rownames(groups)
+      groups <- dplyr::left_join(groups, print_position_calc)
+    } else if (print_position == "mean") {
+      print_position_calc <- df %>%
+        dplyr::group_by(!!x) %>%
+        dplyr::summarise(!!y := mean(!!y, na.rm = TRUE) + print_adjust * raw_sd$sd)
+      groups[[dplyr::quo_name(x)]] <- rownames(groups)
+      groups <- dplyr::left_join(groups, print_position_calc)
+    } else if (print_position == "below") {
+      print_position_calc <- df %>%
+        dplyr::group_by(!!x) %>%
+        dplyr::summarise(!!y := min(!!y, na.rm = TRUE) - print_adjust * raw_sd$sd)
+      groups[[dplyr::quo_name(x)]] <- rownames(groups)
+      groups <- dplyr::left_join(groups, print_position_calc)
+    } else {
+      groups <- groups %>%
+        tibble::rownames_to_column(var = quo_name(x)) %>%
+        mutate(!!y := print_position)
+    }
+    groups <- groups %>% 
+      dplyr::mutate(!!x := stringr::str_replace_all(!!x, "PLACEHOLDER", fixed("-")))
+    return(groups)
   }
-  raw_sd <- df %>% dplyr::summarise(sd = sd(!!y, na.rm = TRUE))
-  if (print_position == "above") {
-    print_position_calc <- df %>%
-      dplyr::group_by(!!x) %>%
-      dplyr::summarise(!!y := max(!!y, na.rm = TRUE) + print_adjust * raw_sd$sd)
-    groups[[dplyr::quo_name(x)]] <- rownames(groups)
-    groups <- dplyr::left_join(groups, print_position_calc)
-  } else if (print_position == "mean") {
-    print_position_calc <- df %>%
-      dplyr::group_by(!!x) %>%
-      dplyr::summarise(!!y := mean(!!y, na.rm = TRUE) + print_adjust * raw_sd$sd)
-    groups[[dplyr::quo_name(x)]] <- rownames(groups)
-    groups <- dplyr::left_join(groups, print_position_calc)
-  } else if (print_position == "below") {
-    print_position_calc <- df %>%
-      dplyr::group_by(!!x) %>%
-      dplyr::summarise(!!y := min(!!y, na.rm = TRUE) - print_adjust * raw_sd$sd)
-    groups[[dplyr::quo_name(x)]] <- rownames(groups)
-    groups <- dplyr::left_join(groups, print_position_calc)
-  } else {
-    groups <- groups %>%
-      tibble::rownames_to_column(var = quo_name(x)) %>%
-      mutate(!!y := print_position)
-  }
-  return(groups)
 }
--- R/letter_groups.R
+++ R/letter_groups.R
@@ -11,13 +11,13 @@
 #' @param print_adjust Adjustment of the letter position multiples of the overall standard deviations. Defaults to 1.
 #' @param stat_alpha The significance threshold alpha. Defaults to 0.05.
 #' @param p_adj_method Method for p value adjustment, see ?dunn.test::dunn.test for details. One of "none", "bonferroni", "sidak", "holm", "hs", "hochberg", "bh", "by". Defaults to "holm".
+#' @param table A logical value indicating whether to return a data frame with the letter codes according to significance (table = FALSE, default), or a table containing the calculated statistics (P-values, df, etc.).
 #' @keywords Kruskal-Wallis, Tukey HSD, annotation
 #' @export
 #' @examples
 #' test_data <- tibble(value = rnorm(1000), sample = sample(LETTERS[1:5], 1000, replace = TRUE), group = sample(LETTERS[24:26], 1000, replace = TRUE))
-#' annotation_df <- letter_groups(test_data, value, sample, "tukey", group, stat_alpha = 0.001, print_position = "above", print_adjust = 0.5, p_adj_method = "holm")
-letter_groups <- function(
-                          df,
+#' annotation_df <- letter_groups(test_data, value, sample, "tukey", group, stat_alpha = 0.001, print_position = "above", print_adjust = 0.5, p_adj_method = "holm", table = FALSE)
+letter_groups <- function(df,
                           y,
                           x,
                           stat_method,
@@ -25,7 +25,8 @@ letter_groups <- function(
                           print_position = 0,
                           print_adjust = 1,
                           stat_alpha = 0.05,
-                          p_adj_method = "holm") {
+                          p_adj_method = "holm",
+                          table = FALSE) {
   y <- dplyr::enquo(y)
   x <- dplyr::enquo(x)
   grp <- dplyr::enquos(...)
@@ -34,6 +35,5 @@ letter_groups <- function(
     drop_na(!!x, !!y, !!!grp) %>%
     dplyr::mutate(!!x := stringr::str_replace_all(!!x, fixed("-"), "PLACEHOLDER")) %>%
     dplyr::group_by(!!!grp) %>%
-    dplyr::group_modify(~ data.frame(internal_stats(., y, x, stat_method, print_position, print_adjust, stat_alpha, p_adj_method))) %>%
-    dplyr::mutate(!!x := stringr::str_replace_all(!!x, "PLACEHOLDER", fixed("-")))
+    dplyr::group_modify(~ data.frame(internal_stats(., y, x, stat_method, print_position, print_adjust, stat_alpha, p_adj_method, table)))
 }
